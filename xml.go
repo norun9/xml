e877de80ecf62447a0fc6ea9a98f15f3be732cac
tomoyaueno29 <u.t012807@gmail.com>
2020-06-07 20:06:55 +0900

    Update xml.go

--- xml.go
+++ xml.go
@@ -27,7 +27,7 @@ func main() {
 			Name: "Sau Sheong",
 		},
 	}
-	output, err := xml.Marshal(&post)
+	output, err := xml.MarshalIndent(&post, "", "\t")
 	if err != nil{
 		fmt.Println("Error marshalling to XML:", err)
 		return
