709045f8090fc9d625c8c1d7eb4372295fc6b88e
tomoyaueno29 <u.t012807@gmail.com>
2020-06-07 19:59:46 +0900

    Update xml.go

--- xml.go
+++ xml.go
@@ -1,1 +1,17 @@
 package main
+
+import "encoding/xml"
+
+type Post struct {
+	XMLName  xml.Name  `xml:"post"`
+	Id       string    `xml:"id,attr"`
+	Content  string    `xml:"content"`
+	Author   string    `xml:"author"`
+	Xml      string    `xml:",innerxml"`
+	Comments []Comment `xml:"comments>comment"`
+}
+
+type Author struct {
+	Id   string `xml:"id",attr`
+	Name string `xml:",chardata"`
+}
