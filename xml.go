e6a9434d739254d9726122a483ebfcf175586e75
tomoyaueno29 <u.t012807@gmail.com>
2020-06-07 20:00:30 +0900

    Update xml.go

--- xml.go
+++ xml.go
@@ -1,6 +1,10 @@
 package main
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+)
 
 type Post struct {
 	XMLName  xml.Name  `xml:"post"`
@@ -14,4 +18,25 @@ type Post struct {
 type Author struct {
 	Id   string `xml:"id",attr`
 	Name string `xml:",chardata"`
+}
+
+func main() {
+	post := Post{
+		Id: "1",
+		Content: "Hello World",
+		Author: Author{
+			Id: "2",
+			Name: "Sau Sheong",
+		},
+	}
+	output, err := xml.Marshal(&post)
+	if err != nil{
+		fmt.Println("Error marshalling to XML:", err)
+		return
+	}
+	err = ioutil.WriteFile("post2.xml", output, 0644)
+	if err != nil{
+		fmt.Println("Error writing XML to file:", err)
+		return
+	}
 }
