3869bdf077e94616ca9212a3d684de9571751a7f
tomoyaueno29 <u.t012807@gmail.com>
2020-06-08 11:26:08 +0900

    Update data.go

--- webService/data.go
+++ webService/data.go
@@ -17,5 +17,6 @@ func init(){
 
 func retrieve(id int) (post Post, err error) {
 	post = Post{}
-	
+	err = Db.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.Id, &post.Content, &post.Author)
+	return
 }
