397d9525925c09955b7d53026355c2c34f64804c
tomoyaueno29 <u.t012807@gmail.com>
2020-06-08 11:50:37 +0900

    Update server.go

--- webService/server.go
+++ webService/server.go
@@ -63,4 +63,32 @@ func handlePost(w http.ResponseWriter, r *http.Request) (err error){
 	body := make([]byte, len)
 	r.Body.Read(body)
 	var post Post
+	json.Unmarshal(body, &post)
+	err = post.create()
+	if err != nil {
+		return
+	}
+	w.WriteHeader(200)
+	return
+}
+
+func handlePut(w http.ResponseWriter, r *http.Request) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	if err != nil {
+		return
+	}
+	post, err := retrieve(id)
+	if err != nil {
+		return
+	}
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+	json.Unmarshal(body, &post)
+	err = post.update()
+	if err != nil {
+		return
+	}
+	w.WriteHeader(200)
+	return
 }
