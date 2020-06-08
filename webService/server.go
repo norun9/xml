ee48ba864c4110546c0eed9ab0320b9ce4eefec9
tomoyaueno29 <u.t012807@gmail.com>
2020-06-08 11:39:47 +0900

    Update server.go

--- webService/server.go
+++ webService/server.go
@@ -12,6 +12,24 @@ func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc()
+	http.HandleFunc("/post", handleRequest)
 	server.ListenAndServe()
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request){
+	var err error
+	switch r.Method {
+	case "GET":
+		err = handleGet(w, r)
+	case "POST":
+		err = handlePost(w, r)
+	case "PUT":
+		err = handlePut(w, r)
+	case "DELETE":
+		err = handleDelete(w, r)
+	}
+
+	if erttp.Errorr != nil {
+		h
+	}
 }
