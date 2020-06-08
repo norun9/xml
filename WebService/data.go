101a059dce9787151a4fa95763724919805bcf52
tomoyaueno29 <u.t012807@gmail.com>
2020-06-08 12:29:37 +0900

    Update data.go

--- WebService/data.go
+++ WebService/data.go
@@ -15,13 +15,13 @@ func init(){
 	}
 }
 
-func retrieve(id int) (post Post, err error) {
+func Retrieve(id int) (post Post, err error) {
 	post = Post{}
 	err = Db.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.Id, &post.Content, &post.Author)
 	return
 }
 
-func (post *Post) create() (err error) {
+func (post *Post) Create() (err error) {
 	statement := "insert into posts (content, author) values ($1, $2) returning id"
 	stmt, err := Db.Prepare(statement)
 	if err != nil {
@@ -32,12 +32,12 @@ func (post *Post) create() (err error) {
 	return
 }
 
-func (post *Post) update() (err error) {
+func (post *Post) Update() (err error) {
 	_, err = Db.Exec("update posts set content = $2, author = $3 where id = $1", post.Id, post.Content, post.Author)
 	return
 }
 
-func (post *Post) delete() (err error) {
+func (post *Post) Delete() (err error) {
 	_, err = Db.Exec("delete from posts where id = $1", post.Id)
 	return
 }
