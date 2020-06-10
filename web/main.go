97991d7142b6fca0ba5eacb5305baee5f5c0438d
tomoyaueno29 <u.t012807@gmail.com>
2020-06-10 15:10:04 +0900

    Update main.go

--- web/main.go
+++ web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 )
@@ -26,19 +27,32 @@ func loadPage(title string) (*Page , error) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request){
-	
+	w.Header().Set("Content-Type", "application/json")
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil{
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
 }
 
 func main() {
-	//p1 := &Page{Title: "test", Body:[]byte("This is a sample page.\t")}
-	//p1.save()
-	//
-	//p2, _ := loadPage(p1.Title)
-	//fmt.Println(string(p2.Body))
-
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc('/view', viewHandler)
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	server.ListenAndServe()
 }
