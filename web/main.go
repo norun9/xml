35275d60333a2c1bfa4e7ce94b9afb8bc4944a96
tomoyaueno29 <u.t012807@gmail.com>
2020-06-10 14:43:04 +0900

    Update main.go

--- web/main.go
+++ web/main.go
@@ -1,8 +1,11 @@
-package web
+package main
 
-import "io/ioutil"
+import (
+	"fmt"
+	"io/ioutil"
+)
 
-type Page struct {
+type Page struct{
 	Title string
 	Body []byte
 }
@@ -12,15 +15,19 @@ func (p *Page) save() (err error) {
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-func loadPage(title string) (*Page ,error) {
+func loadPage(title string) (*Page , error) {
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
-
+		return nil, err
 	}
+	return &Page{Title: title, Body: body}, nil
 }
 
 func main() {
-	p1 := &Post{Title: "test", Body:[]byte("This is a sample page.\t")}
+	p1 := &Page{Title: "test", Body:[]byte("This is a sample page.\t")}
 	p1.save()
+
+	p2, _ := loadPage(p1.Title)
+	fmt.Println(string(p2.Body))
 }
